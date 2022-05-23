ea85bbf1dec5e7a0570ba5a194b33e925182a968
novifitri <animangap@gmail.com>
2022-05-23 20:26:15 +0700

    init

---
+++ tugas19.js
@@ -0,0 +1,13 @@
+//soal 1 buat perulangan kata
+console.log("Semangat...".repeat(5))
+
+//soal 2 buat perulangan bintang sesuai cth gambar
+console.log("*".repeat(5))
+console.log("*".repeat(25))
+console.log("*".repeat(45))
+console.log("*".repeat(65))
+console.log("*".repeat(95))
+console.log("*".repeat(65))
+console.log("*".repeat(45))
+console.log("*".repeat(25))
+console.log("*".repeat(5))
